5abd5582801f9e7256bb730835de9c07575df76b
SidK123 <83468167+SidK123@users.noreply.github.com>
2023-10-02 19:55:10 -0400

    Update calculator.sv

--- src/calculator.sv
+++ src/calculator.sv
@@ -3,10 +3,14 @@ module tt_um_calculator_chip (
     input logic [7:0] NumIn,
     input logic [1:0] OpIn,
     input logic Enter,
-    input logic ena,
-    input logic rst_n,
-
-    input logic clk
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
     enum logic [2:0] {START, CALCULATE1, CALCULATE2, CALCULATE3, CALCULATE4, WAIT} currState, nextState1;
